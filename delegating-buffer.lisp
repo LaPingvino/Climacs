0052cdaf45fb3b2d923216b8275171a619b99951
Aleksandar Bakic <abakic@common-lisp.net>
2005-02-27 21:21:51 +0000

    Fixed description...

--- delegating-buffer.lisp
+++ delegating-buffer.lisp
@@ -18,7 +18,7 @@
 ;;; Free Software Foundation, Inc., 59 Temple Place - Suite 330,
 ;;; Boston, MA  02111-1307  USA.
 
-;;; Buffer class that allow specifying buffer implementation at run time.
+;;; Buffer class that allows for specifying buffer implementation at run time.
 
 (in-package :climacs-buffer)
 
