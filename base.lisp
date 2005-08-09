ba4efff98c1a84d71d464154da39192b656ea785
Dave Murray <dmurray@common-lisp.net>
2005-08-09 15:18:25 +0000

    Removed sb-impl::whitespacep, which is disappearing.

--- base.lisp
+++ base.lisp
@@ -185,8 +185,7 @@ one of the marks"))
 (defun whitespacep (obj)
   "A predicate to ensure that an object is a whitespace character."
   (and (characterp obj)
-       #+sbcl (sb-impl::whitespacep obj)
-       #-sbcl (member obj '(#\Space #\Tab #\Newline #\Page))))
+       (member obj '(#\Space #\Tab #\Newline #\Page #\Return))))
 
 (defun forward-to-word-boundary (mark)
   "Move the mark forward to the beginning of the next word."
